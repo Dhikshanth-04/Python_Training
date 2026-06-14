9a342c8bd56f61d17513b33ce2f17074827075dc
Dhikshanth-04 <squadattack9@gmail.com>
2026-06-14 19:37:46 +0530

    print all subarray

---
+++ python_basic/Subarrays.py
@@ -0,0 +1,12 @@
+def subarray(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(i, n):
+            print(arr[i:j+1])
+
+arr = []
+size = int(input("Enter array size : "))
+for i in range(size):
+    arr.append(int(input("Enter array element : ")))
+subarray(arr)
